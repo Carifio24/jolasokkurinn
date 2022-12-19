a41ad747b1e03d1b9b0bcfb3845b6668bbed83cb
Jon Carifio <carifio24@gmail.com>
2022-12-19 15:03:00 -0500

    Initial commit.

---
+++ assignments/assignments.js
@@ -0,0 +1,65 @@
+const sjcl = require("sjcl");
+
+function shuffle(a) {
+    let j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+function rotate(array, n) {
+    n = n % array.length;
+    return array.slice(n, array.length).concat(array.slice(0, n));
+}
+
+function randomOffsets(size, max) {
+    const indices = [];
+    while (indices.length < size) {
+        const value = Math.ceil(Math.random() * max);
+        if (!indices.includes(value)) {
+            indices.push(value);
+        }
+    }
+    return indices;
+}
+
+function offsetsValid(offsets, max) {
+    for (let i = 1; i < max; i++) {
+        const shifted = offsets.map(x => (x + i) % max);
+        if (shifted.every(x => offsets.includes(x))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function createPresentsSets(names, setSize) {
+    const shuffled = shuffle(names);
+    const nameCount = shuffled.length;
+
+    // We check if this set of offsets is "valid"
+    // which for now we define to mean that no sets
+    // are repeated
+    let offsets = randomOffsets(setSize, nameCount);
+    while (!offsetsValid(offsets, nameCount)) {
+        offsets = randomOffsets(setSize, nameCount);
+    }
+
+    const rotatedArrays = offsets.map(offset => rotate(shuffled, offset));
+    const result = {};
+    names.forEach((name, index) => {
+        result[name] = shuffle(rotatedArrays.map(array => array[index]));
+    });
+    return result;
+
+}
+
+const names = ["Elna", "Gudfridur", "Stefania", "Kristjan", "Loa", "Jon", "Diddi"];
+const setSize = 3;
+const results = createPresentsSets(names, setSize);
+const encrypted = sjcl.encrypt("pEg7e31cNASh", JSON.stringify(results));
+console.log(encrypted);
---
+++ script.js
@@ -0,0 +1,44 @@
+function handleResize(_event) {
+    const textOffset = 410;
+    const pictureWidth = 1050;
+    const marginWidths = (window.innerWidth - pictureWidth) / 2;
+    const posX = marginWidths + textOffset;
+    const list = document.querySelector("#list-content");
+    list.style.left = `${posX}px`;
+}
+
+handleResize();
+
+const sjclScript = document.createElement("script");
+sjclScript.src = "http://bitwiseshiftleft.github.io/sjcl/sjcl.js";
+sjclScript.async = false;
+sjclScript.onload = () => {
+    document.body.removeChild(sjclScript);
+};
+document.body.appendChild(sjclScript);
+
+
+const presentsData = {
+    Elna: [ 'Stefania', 'Kristjan', 'Loa' ],
+    Diddi: [ 'Kristjan', 'Gudfridur', 'Elna' ],
+    Jon: [ 'Loa', 'Diddi', 'Gudfridur' ],
+    Stefania: [ 'Jon', 'Loa', 'Elna' ],
+    Kristjan: [ 'Stefania', 'Elna', 'Diddi' ],
+    Gudfridur: [ 'Kristjan', 'Diddi', 'Jon' ],
+    Loa: [ 'Stefania', 'Jon', 'Gudfridur' ]
+  };  
+
+const params = new URLSearchParams(window.location.search);
+const whoEncoded = params.get("who");
+console.log(whoEncoded);
+if (whoEncoded) {
+    const who = atob(whoEncoded);
+    const names = presentsData[who];
+
+    for (let i = 0; i < names.length; i++) {
+        const el = document.querySelector(`#name-${i+1}`);
+        el.textContent = names[i];
+    }
+}
+
+window.addEventListener('resize', handleResize);
